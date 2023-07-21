053c510e56db125463f163a280364254180e5693
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-07-21 09:31:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,7 +76,6 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
-HOW_TO_VERIFY = environ.get("HOW_TO_VERIFY", "https://t.me/+W5plh7_tP19lZjg1")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'TinyFy.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '8f29e4130ed3439c4c5cfc5c56dae86c0aff0daa')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
